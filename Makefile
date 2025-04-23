63eefe297b142cefe37503809634e4a911643934
Dirk Moeller <moeller.dirk80@gmail.com>
2025-04-23 21:09:03 +0200

    add simple keybinding script

--- Makefile
+++ Makefile
@@ -37,9 +37,13 @@ install: all
 	mkdir -p ${DESTDIR}${MANPREFIX}/man1
 	sed "s/VERSION/${VERSION}/g" < dwm.1 > ${DESTDIR}${MANPREFIX}/man1/dwm.1
 	chmod 644 ${DESTDIR}${MANPREFIX}/man1/dwm.1
+	mkdir -p ${DESTDIR}${PREFIX}/share/dwm
+	cp -f config.h dwm_keys.py ${DESTDIR}${PREFIX}/share/dwm
+	chmod 755 ${DESTDIR}${PREFIX}/share/dwm/dwm_keys.py
 
 uninstall:
 	rm -f ${DESTDIR}${PREFIX}/bin/dwm\
 		${DESTDIR}${MANPREFIX}/man1/dwm.1
+	rm -rf ${DESTDIR}${PREFIX}/share/dwm
 
 .PHONY: all clean dist install uninstall
---
+++ dwm_keys.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+
+# stupid script that collects key shortcut comments and prints a summary on stdout.
+# it assumes config.h in the same directory.
+
+lines = []
+
+with open("config.h") as f:
+    start = False
+    for l in f.readlines():
+        if start:
+            if l.find("};") == 0:
+                break
+            l = l.strip()
+            if l.find("{") == 0:
+                l = l[1:]
+                secondcomma = l.find(",", l.find(",")+1)
+                key = l[:secondcomma]
+                key = key.replace(",", "+")
+                key = key.replace("|", "+")
+                key = key.replace(" ", "")
+                key = key.replace("XK_", "")
+                key = key.replace("MODKEY", "Super")
+                key = key.replace("ShiftMask", "Shift")
+                key = key.replace("ControlMask", "Ctrl")
+                commentpos = l.find("//")
+                comment = ""
+                if commentpos > 0:
+                    comment = ": " + l[commentpos+2:].strip()
+                    print(f"{key:20} {comment}")
+                else:
+                    print(key)
+        elif l.find("static const Key keys[] = {") == 0:
+            start = True
